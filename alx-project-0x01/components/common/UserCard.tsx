cab0c3bc29932f0977fa9c99092a2ea9863fd4cd
faithakinbobola <faithakinbobola2@gmail.com>
2025-07-20 01:36:36 +0100

    Implement Users Card Components

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,67 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({ id, name, username, email, address, phone, website, company }) => {
+    return (
+        <div className="w-4/5 rounded-xl  p-5 shadow-lg bg-white mx-auto" >
+            <div className="pb-4">
+                <h1 className="text-2xl font-bold">{name}</h1>
+                <h2 className="text-gray-600">@{username}</h2>
+                <p className="text-gray-500">{email}</p>
+            </div>
+            <table className="table-auto w-full bg-gray-100 rounded-lg border">
+                <thead>
+                    <tr>
+                        <th className="font-bold p-2 text-left">Personal details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="p-2 text-gray-500">Fullname:</td>
+                        <td className="p-2 font-semibold">{name}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Email:</td>
+                        <td className="p-2 font-semibold">{email}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Phone:</td>
+                        <td className="p-2 font-semibold">{phone}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Website:</td>
+                        <td className="p-2 font-semibold">{website}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Address:</td>
+                        <td className="p-2 font-semibold">{address.street}, {address.suite} {address.city}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Zip:</td>
+                        <td className="p-2 font-semibold">{address.zipcode}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Geo:</td>
+                        <td className="p-2 font-semibold">{address.geo.lat}, {address.geo.lng}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Company:</td>
+                        <td className="p-2 font-semibold">{company.name}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Company Catch Phrase:</td>
+                        <td className="p-2 font-semibold">{company.catchPhrase}</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 text-gray-500">Company B.S:</td>
+                        <td className="p-2 font-semibold">{company.bs}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <p>User Id: {id}</p>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,35 @@
-const Users: React.FC = () => {
+import UserCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces"
+
+const Users: React.FC<UserProps[]> = ({ posts }) => {
+    console.log(posts);
+    
     return (
-        <div>
-            Users
+        <div className="flex flex-col h-screen">
+            <Header />
+            <main className="p-4">
+                <div className="grid grid-cols-2 gap-6">
+                    {
+                        posts?.map(({ id, name, username, email, address, phone, website, company }: UserProps, key: number) => (
+                            <UserCard name={name} username={username} email={email} address={address} phone={phone} website={website} company={company} id={id} key={key} />
+                        ))
+                    }
+                </div>
+            </main>
         </div>
     )
 }
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Users;
